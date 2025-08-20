c2c2575c25e6c8dc9c7dda454f2bd36f2c039309
abdullahalnomandev <abdullahalnoman1512@gmail.com>
2025-08-20 14:44:37 +0600

    env updated

--- next.config.ts
+++ next.config.ts
@@ -8,10 +8,7 @@ const nextConfig = {
   },
   typescript: {
     ignoreBuildErrors: true,
-  },
-  experimental: {
-    missingSuspenseWithCSRBailout: false,
-  },
+  }
 };
 
 export default nextConfig;
